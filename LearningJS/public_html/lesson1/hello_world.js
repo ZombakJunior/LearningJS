75b4c5eb09764cfe3483344564ebea9601a9acc4
jvanc <jvanc@192.168.1.180>
2017-10-08 18:11:32 +0200

    First lesson in JS

---
+++ LearningJS/public_html/lesson1/hello_world.js
@@ -0,0 +1,6 @@
+function hello()
+{
+    document.getElementById("sample_topic").innerHTML = 'Hello world';
+    document.getElementById("sample_text").innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/OfaBZvvL_7M"></iframe>';
+    document.getElementById('sample_button').style.visibility = 'hidden';
+}
