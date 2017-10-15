354ad3e4d5b973e1058bb2eb3b71778b75a8f3e6
zombak79 <31569254+zombak79@users.noreply.github.com>
2017-10-15 18:08:59 +0200

    Commentary on lesson 1

--- LearningJS/public_html/lesson1/hello_world.js
+++ LearningJS/public_html/lesson1/hello_world.js
@@ -1,6 +1,10 @@
 function hello()
 {
     document.getElementById("sample_topic").innerHTML = 'Hello world';
-    document.getElementById("sample_text").innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/OfaBZvvL_7M"></iframe>';
+    document.getElementById("sample_text").innerHTML = '<iframe \n\
+                                                            width="420" \n\
+                                                            height="315" \n\
+                                                            src="https://www.youtube.com/embed/OfaBZvvL_7M">\n\
+                                                        </iframe>';
     document.getElementById('sample_button').style.visibility = 'hidden';
 }
